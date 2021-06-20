446e2dc303e623b4f2aa7ee8af75ae858b46cfd6
aya-alhusamia <alhusamiaya70@gmail.com>
2021-06-20 23:51:46 +0300

    event

--- db/models/Event.js
+++ db/models/Event.js
@@ -1,43 +1,38 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("Event", {
-        organizer: {
-        type: DataTypes.STRING,
-        len: [0, 20],
-        unique: true
-      },
-       name :{
-        type: DataTypes.STRING,
-        validate: 
-         { notContains : "event"}
-       },
+  return sequelize.define("Event", {
+    organizer: {
+      type: DataTypes.STRING,
+      len: [0, 20],
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: { notContains: "event" },
+    },
 
-      email: {
-        type: DataTypes.STRING,
-        validateValue: {isEmail: true, 
-        allowNull:true}
-
-      }, 
-      image: {
-        type: DataTypes.STRING,
-        validateValue:{allowNull: true}
-      },
-      numOfSeats: {
-        type: DataTypes.INTEGER,
-        validValue:{ min: 1}
-
-      },
-      bookedSeats : {
-        type: DataTypes.INTEGER,
-        validValue: {max: 5}
-        
-      },
-      startDate: {
-        type: DataTypes.DATE,
-       isAfter: "2021-06-20",
-      },
-      endDate: {
-        type: DataTypes.DATE
-
-      }
-    });
-  }
+    email: {
+      type: DataTypes.STRING,
+      validateValue: { isEmail: true, allowNull: false },
+    },
+    image: {
+      type: DataTypes.STRING,
+      validateValue: { allowNull: false },
+    },
+    numOfSeats: {
+      type: DataTypes.INTEGER,
+      validValue: { min: 0 },
+    },
+    bookedSeats: {
+      type: DataTypes.INTEGER,
+      max: Event.numOfSeats,
+    },
+    startDate: {
+      type: DataTypes.DATE,
+      isAfter: "2021-06-20",
+    },
+    endDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  });
+};
