c35f1e311c917570ef9fd93f1dab3bdaaf43fa9f
IsmaelAlomari <ismaelalomari994@gmail.com>
2021-06-20 18:19:28 +0300

    still

---
+++ db/models/Event.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define("Event", {
+        organizer: {
+        type: DataTypes.STRING,
+        len: [0, 20],
+        unique: true
+      },
+       name :{
+        type: DataTypes.STRING,
+        validate: {
+          not: /^[event]+$/i
+       },
+
+      email: {
+        type: DataTypes.STRING,
+        validateValue: {isEmail: true, 
+        allowNull:true}
+
+      }, 
+      image: {
+        type: DataTypes.STRING,
+        validateValue:{allowNull: true}
+      },
+      numOfSeats: {
+        type: DataTypes.INTEGER,
+        validValue:{ min: 1}
+
+      },
+      bookedSeats : {
+        type: DataTypes.INTEGER,
+        validValue: {max: 5}
+        }
+      },
+      startDate: {
+        type: DataTypes.DATE,
+       isAfter: "2021-06-20",
+      },
+      endDate: {
+        type: DataTypes.DATE
+
+      }
+    });
+  }
